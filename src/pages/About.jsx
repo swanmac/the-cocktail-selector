0517fed926487b077aaaa0615d31e4977870b266
Greg Swan <gmswan99@gmail.com>
2023-06-05 19:53:18 -0400

    add about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
     <section className="section about-section">
       <h1 className="section-title">about us</h1>
       <p>
-        Thank you for visiting our site, the Adult Beverage Selector. In the search box, just enter a letter that is in the name of your favorite adult beverage and you will see a list of suggested beverages. Select one of the adult beverages and you will be provided with additional details about the beverage including: the category, the glass, ingredients and instructions on how to prepare the beverage. Enjoy!!
+        Thank you for visiting our site, The Cocktail Selector. In the search box, just enter a letter that is in the name of your favorite adult beverage and you will see a list of suggested beverages. Select the details button on any of the cocktails and you will be provided with additional details about the beverage including: the category, the cocktail glass, ingredients and instructions on how to prepare the beverage. Enjoy!!
     
       </p>
     </section>
