395751876b121cef006122be200a32c1f1b3d71b
Greg Swan <gmswan99@gmail.com>
2023-06-05 19:48:06 -0400

    first commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Thank you for visiting our site, the Adult Beverage Selector. In the search box, just enter a letter that is in the name of your favorite adult beverage and you will see a list of suggested beverages. Select one of the adult beverages and you will be provided with additional details about the beverage including: the category, the glass, ingredients and instructions on how to prepare the beverage. Enjoy!!
+    
+      </p>
+    </section>
+  );
+}
